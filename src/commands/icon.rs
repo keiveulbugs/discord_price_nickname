ba5185a8816cb4bc08b1e6aa191d1e2216466db9
keiveulbugs <114590495+keiveulbugs@users.noreply.github.com>
2023-03-25 15:17:34 +0100

    it now updates the bot avatar, although its a bit dirty

--- src/commands/icon.rs
+++ src/commands/icon.rs
@@ -1,19 +1,31 @@
 use crate::Error;
-use poise::serenity_prelude::{self as serenit, ChannelId, Attachment};
+use poise::serenity_prelude::{self as serenit, ChannelId, Attachment, CacheHttp, Context};
 use serenity::utils::Colour;
+use std::io;
+use std::fs::File;
+use std::io::Cursor;
+use poise::serenity_prelude::EditProfile;
+
 
 /// About command
 #[poise::command(slash_command)]
 pub async fn icon(ctx: poise::Context<'_, (), Error>,
-#[description = "Enter the smart contract address of the pair"] icon: Attachment,
+#[description = "Upload a logo"] icon: Attachment,
 ) -> Result<(), Error> {
     //ctx.send("hello").await?;
+   
 
-    let file = &icon.url;
-    let bitsandbytes = reqwest::get(file).await?;
-    let bitsies = bitsandbytes.bytes().await?;
-    println!("{}", file);
-    println!("{:#?}", icon);
+    let fileurl = &icon.url;
+    let response = reqwest::get(fileurl).await?;
+    let mut file = std::fs::File::create(&icon.filename)?;
+    let mut content =  Cursor::new(response.bytes().await?);
+    std::io::copy(&mut content, &mut file)?;
+    let base64 = serenity::utils::read_image(&icon.filename)?;
+    let mut user = ctx.cache().unwrap().current_user();
+    let _ = user.edit(&ctx, |p| p.avatar(Some(&base64))).await;
+    std::fs::remove_file(icon.filename).unwrap();
+    
+    ctx.say("Succesfully set avatar").await?;
 
     Ok(())
 }
